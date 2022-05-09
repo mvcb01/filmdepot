696b71a884971bdbd5f4d0cfc4e53d72f0fdc11f
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-09 18:54:11 +0100

    new FilmCRUD proj

---
+++ FilmCRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilmCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
