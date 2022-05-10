a6da6257334fb759cc8fc7df320fee9c2ee0dbb4
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-10 16:39:26 +0100

    moviedb properties

--- MovieAPIClients/TheMovieDb/TheMovieDbAPIClient.cs
+++ MovieAPIClients/TheMovieDb/TheMovieDbAPIClient.cs
@@ -2,6 +2,16 @@ namespace MovieAPIClients.TheMovieDb
 {
     public class TheMovieDbAPIClient
     {
+        private string _apiKey { get; init; }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public const string MovieDbBaseAddress = "https://api.themoviedb.org/3/";
+
+        public TheMovieDbAPIClient(string apiKey)
+        {
+            this._apiKey = apiKey;
+        }
 
     }
 }
