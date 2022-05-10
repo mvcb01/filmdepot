92ffcdc0d827ae2320824f6a65920ce1cb2ee40e
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-10 13:46:19 +0100

    deleted actors and directors from filmdataaccess.efcore

--- FilmDataAccess.EFCore/SQLiteAppContext.cs
+++ FilmDataAccess.EFCore/SQLiteAppContext.cs
@@ -11,10 +11,6 @@ namespace FilmDataAccess.EFCore
 
         public DbSet<Genre> Genres { get; set; }
 
-        public DbSet<Director> Directors { get; set; }
-
-        public DbSet<Actor> Actors { get; set; }
-
         public DbSet<MovieRip> MovieRips { get; set; }
 
         public DbSet<MovieWarehouseVisit> MovieWarehouseVisits { get; set; }
@@ -29,11 +25,9 @@ namespace FilmDataAccess.EFCore
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration<Movie>(new MovieEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration<Director>(new DirectorEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration<Genre>(new GenreEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration<MovieRip>(new MovieRipEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration<MovieWarehouseVisit>(new MovieWarehouseVisitEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration<Actor>(new ActorEntityTypeConfiguration());
         }
 
     }
--- FilmDataAccess.EFCore/UnitOfWork/SQLiteUnitOfWork.cs
+++ FilmDataAccess.EFCore/UnitOfWork/SQLiteUnitOfWork.cs
@@ -9,25 +9,19 @@ namespace FilmDataAccess.EFCore.UnitOfWork
 
         public IMovieRepository Movies { get; init; }
 
-        public IDirectorRepository Directors { get; init; }
-
         public IGenreRepository Genres { get; init; }
 
         public IMovieRipRepository MovieRips { get; init; }
 
         public IMovieWarehouseVisitRepository MovieWarehouseVisits { get; init; }
 
-        public IActorRepository Actors { get; init; }
-
         public SQLiteUnitOfWork(SQLiteAppContext context)
         {
             _context = context;
             Movies = new MovieRepository(_context);
-            Directors = new DirectorRepository(_context);
             Genres = new GenreRepository(_context);
             MovieRips = new MovieRipRepository(_context);
             MovieWarehouseVisits = new MovieWarehouseVisitRepository(_context);
-            Actors = new ActorRepository(_context);
         }
 
         public int Complete()
