bddbc7fecd954f959cee18a6461809f6b802c220
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-10 16:28:27 +0100

    new project: MovieAPIClients

---
+++ MovieAPIClients/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieAPIClients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
