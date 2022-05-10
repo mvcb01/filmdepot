1d363a716d043a79e33ae9e69c5c765ccaa730da
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-10 14:15:39 +0100

    new entity Person

---
+++ FilmDomain/Entities/Person.cs
@@ -0,0 +1,11 @@
+namespace FilmDomain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string KnownFor { get; set; }
+    }
+}
