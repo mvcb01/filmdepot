d3925133965c72a0bd54c3459d0b58dc85980e84
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-13 16:49:53 +0100

    new migration

---
+++ FilmDataAccess.EFCore/Migrations/20220513154910_ExternalIdIsAlternateKey.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmDataAccess.EFCore.Migrations
+{
+    public partial class ExternalIdIsAlternateKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Directors_Name",
+                table: "Directors");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Actors_Name",
+                table: "Actors");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Directors_ExternalId",
+                table: "Directors",
+                column: "ExternalId");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Actors_ExternalId",
+                table: "Actors",
+                column: "ExternalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Directors_ExternalId",
+                table: "Directors");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Actors_ExternalId",
+                table: "Actors");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Directors_Name",
+                table: "Directors",
+                column: "Name");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Actors_Name",
+                table: "Actors",
+                column: "Name");
+        }
+    }
+}
