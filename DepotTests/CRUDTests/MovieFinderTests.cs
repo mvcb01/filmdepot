2cf5b5f03ef33babd3441c7e66c249ec215deeb1
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-13 17:37:37 +0100

    testclass: MovieFinderTests

---
+++ DepotTests/CRUDTests/MovieFinderTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DepotTests.CRUDTests
+{
+    public class MovieFinderTests
+    {
+        [Fact]
+        public void Test_Something()
+        {
+            {}
+        }
+    }
+}
