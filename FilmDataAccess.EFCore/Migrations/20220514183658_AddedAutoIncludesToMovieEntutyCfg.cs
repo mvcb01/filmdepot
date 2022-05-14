9aefd3488ca5b33e36a726430368d1a590c180f1
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-14 19:37:39 +0100

    new migration

---
+++ FilmDataAccess.EFCore/Migrations/20220514183658_AddedAutoIncludesToMovieEntutyCfg.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmDataAccess.EFCore.Migrations
+{
+    public partial class AddedAutoIncludesToMovieEntutyCfg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
