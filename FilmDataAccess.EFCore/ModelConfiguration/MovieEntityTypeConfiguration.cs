7699d2f6c5f8744f5485382946eda6bfe10151d5
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-14 19:34:45 +0100

    MovieEntityTypeConfiguration auto includes: Directors, Actors, Genres, MovieRips

--- FilmDataAccess.EFCore/ModelConfiguration/MovieEntityTypeConfiguration.cs
+++ FilmDataAccess.EFCore/ModelConfiguration/MovieEntityTypeConfiguration.cs
@@ -11,7 +11,12 @@ namespace FilmDataAccess.EFCore.ModelConfiguration
             builder.Property(m => m.Title).IsRequired();
 
             // a combinação (título, título original, release date) deve identificar bem um filme
-            builder.HasAlternateKey(m => new {m.Title, m.OriginalTitle, m.ReleaseDate });
+            builder.HasAlternateKey(m => new { m.Title, m.OriginalTitle, m.ReleaseDate });
+
+            builder.Navigation(m => m.Actors).AutoInclude();
+            builder.Navigation(m => m.Directors).AutoInclude();
+            builder.Navigation(m => m.Genres).AutoInclude();
+            builder.Navigation(m => m.MovieRips).AutoInclude();
         }
     }
 }
