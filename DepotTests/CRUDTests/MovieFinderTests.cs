3a18005dfdac721dd974ae34deb6dbe86b5a8b2f
mvcb01 <105233935+mvcb01@users.noreply.github.com>
2022-05-15 18:46:37 +0100

    Find movies from rip filenames (#8)
    * moviefinder class
    
    * testclass: MovieFinderTests
    
    * added MovieApiClients proj reference to FilmCRUD
    
    * MovieFinderTests: failing unittests implemented
    
    * class MovieFinder: unittests are passing for FindMovieOnlineAsync
    
    * fixed variable call
    
    * removed unnecessary using
    
    * FindMovieOnlineAsync: filters by exact token match
    
    * formatting
    
    * more formatting
    
    * FindMovieOnlineAsync: OriginalTitle also considered when filtering
    
    * formatting
    
    * formatting
    
    * unitofwork and appsettingsmanager not needed in MovieFinder class

---
+++ DepotTests/CRUDTests/MovieFinderTests.cs
@@ -0,0 +1,179 @@
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+using MovieAPIClients;
+using ConfigUtils.Interfaces;
+using MovieAPIClients.Interfaces;
+using FilmDomain.Interfaces;
+using FilmDomain.Entities;
+using FilmCRUD;
+using FilmCRUD.CustomExceptions;
+
+namespace DepotTests.CRUDTests
+{
+    public class MovieFinderTests
+    {
+        private readonly Mock<IMovieAPIClient> _movieAPIClientMock;
+
+        private readonly MovieFinder _movieFinder;
+
+        public MovieFinderTests()
+        {
+            this._movieAPIClientMock = new Mock<IMovieAPIClient>();
+            this._movieFinder = new MovieFinder(this._movieAPIClientMock.Object);
+        }
+
+        [Fact]
+        public void FindMovieOnlineAsync_WithNoSearchResults_ShouldThrowNoSearchResultsError()
+        {
+            // arrange
+            string titleWithNoResults = "Some movie";
+            this._movieAPIClientMock
+                .Setup(m => m.SearchMovieAsync(It.IsAny<string>()))
+                .ReturnsAsync(new MovieSearchResult[] { });
+
+            // act
+            // nada a fazer
+
+            // assert
+            this._movieFinder
+                .Invoking(m => m.FindMovieOnlineAsync(titleWithNoResults))
+                .Should()
+                .Throw<NoSearchResultsError>();
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResultsAndWithoutProvidedReleaseDate_ShouldThrowMultipleSearchResultsError()
+        {
+            // arrange
+            string movieTitleToSearch = "the fly";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { Title = "The Fly", ReleaseDate = 1986 },
+                new MovieSearchResult()  { Title = "The Fly", ReleaseDate = 1958 },
+                };
+            this._movieAPIClientMock
+                .Setup(m => m.SearchMovieAsync(It.IsAny<string>()))
+                .ReturnsAsync(searchResults);
+
+            // act
+            // nada a fazer
+
+            // assert
+            await this._movieFinder
+                .Invoking(m => m.FindMovieOnlineAsync(movieTitleToSearch))
+                .Should()
+                .ThrowAsync<MultipleSearchResultsError>();
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResultsAndProvidedReleaseDate_ShouldReturnMatchingMovieRip()
+        {
+            // arrange
+            string movieTitleToSearch = "the fly";
+            string movieReleaseDateToSearch = "1986";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { Title = "The Fly", ReleaseDate = 1986 },
+                new MovieSearchResult()  { Title = "The Fly", ReleaseDate = 1958 },
+                };
+            this._movieAPIClientMock
+                .Setup(m => m.SearchMovieAsync(movieTitleToSearch))
+                .ReturnsAsync(searchResults);
+
+            // act
+            Movie movieFound = await this._movieFinder.FindMovieOnlineAsync(movieTitleToSearch, movieReleaseDateToSearch);
+
+            // assert
+            // só nos interessa o valor das duas properties Title e ReleaseDate
+            movieFound
+                .Should()
+                .BeEquivalentTo(new { Title = "The Fly", ReleaseDate = 1986 });
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResultsAndProvidedReleaseDateWithoutMatch_ShouldThrowNoSearchResultsError()
+        {
+            // arrange
+            string movieTitleToSearch = "the fly";
+            string movieReleaseDateToSearch = "1900";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { Title = "The Fly", ReleaseDate = 1986, ExternalId = 1 },
+                new MovieSearchResult()  { Title = "The Fly", ReleaseDate = 1958, ExternalId = 2 },
+                };
+            this._movieAPIClientMock.Setup(m => m.SearchMovieAsync(movieTitleToSearch)).ReturnsAsync(searchResults);
+
+            // act
+            // nada a fazer
+
+            // assert
+            await this._movieFinder
+                .Invoking(m => m.FindMovieOnlineAsync(movieTitleToSearch, movieReleaseDateToSearch))
+                .Should()
+                .ThrowAsync<NoSearchResultsError>();
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResultsWithSameReleaseDate_ShouldThrowMultipleSearchResultsError()
+        {
+            // arrange
+            string movieTitleToSearch = "the fly";
+            string movieReleaseDateToSearch = "1986";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { Title = "The Fly", ReleaseDate = 1986, ExternalId = 1 },
+                new MovieSearchResult()  { Title = "The Fly", ReleaseDate = 1986, ExternalId = 2 },
+                };
+            this._movieAPIClientMock.Setup(m => m.SearchMovieAsync(movieTitleToSearch)).ReturnsAsync(searchResults);
+
+            // act
+            // nada a fazer
+
+            // assert
+            await this._movieFinder
+                .Invoking(m => m.FindMovieOnlineAsync(movieTitleToSearch, movieReleaseDateToSearch))
+                .Should()
+                .ThrowAsync<MultipleSearchResultsError>();
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResults_ShouldReturnTheResultWithTitleExactMatch()
+        {
+            // arrange
+            string movieTitleToSearch = "sorcerer";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { Title = "Sorcerer", ReleaseDate = 1977},
+                new MovieSearchResult()  { Title = "Highlander III: The Sorcerer", ReleaseDate = 1994},
+                };
+            this._movieAPIClientMock.Setup(m => m.SearchMovieAsync(movieTitleToSearch)).ReturnsAsync(searchResults);
+
+            // act
+            Movie movieFound = await this._movieFinder.FindMovieOnlineAsync(movieTitleToSearch);
+
+            // assert
+            movieFound
+                .Should()
+                .BeEquivalentTo(new { Title = "Sorcerer", ReleaseDate = 1977 });
+        }
+
+        [Fact]
+        public async void FindMovieOnlineAsync_WithSeveralSearchResults_ShouldReturnTheResultWithOriginalTitleExactMatch()
+        {
+            // arrange
+            string movieTitleToSearch = "the fly";
+            MovieSearchResult[] searchResults = {
+                new MovieSearchResult() { OriginalTitle = "The Fly", ReleaseDate = 1986 },
+                new MovieSearchResult()  { Title = "Curse of the Fly", ReleaseDate = 1965 },
+                };
+            this._movieAPIClientMock.Setup(m => m.SearchMovieAsync(movieTitleToSearch)).ReturnsAsync(searchResults);
+
+            // act
+            Movie movieFound = await this._movieFinder.FindMovieOnlineAsync(movieTitleToSearch);
+
+            // assert
+            movieFound
+                .Should()
+                .BeEquivalentTo(new { OriginalTitle = "The Fly", ReleaseDate = 1986 });
+        }
+
+
+    }
+}
---
+++ FilmCRUD/MovieFinder.cs
@@ -0,0 +1,86 @@
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+using FilmCRUD.CustomExceptions;
+using FilmDomain.Entities;
+using MovieAPIClients;
+using ConfigUtils.Interfaces;
+using MovieAPIClients.Interfaces;
+using FilmDomain.Interfaces;
+
+namespace FilmCRUD
+{
+    public class MovieFinder
+    {
+        private IMovieAPIClient _movieAPIClient { get; init; }
+
+        public MovieFinder(IMovieAPIClient movieAPIClient)
+        {
+            this._movieAPIClient = movieAPIClient;
+        }
+
+        public async Task<Movie> FindMovieOnlineAsync(string parsedTitle, string parsedReleaseDate = null)
+        {
+            IEnumerable<MovieSearchResult> searchResultAll = await _movieAPIClient.SearchMovieAsync(parsedTitle);
+
+            // filtra usando Title e OriginalTitle
+            IEnumerable<string> titleTokens = SplitTitleIntoTokens(parsedTitle);
+            List<MovieSearchResult> searchResult = searchResultAll
+                .Where(r => titleTokens.SequenceEqual(SplitTitleIntoTokens(r.Title ?? string.Empty))
+                    ||
+                    titleTokens.SequenceEqual(SplitTitleIntoTokens(r.OriginalTitle ?? string.Empty)))
+                .ToList();
+
+            int resultCount = searchResult.Count();
+            MovieSearchResult result;
+            if (resultCount == 0)
+            {
+                throw new NoSearchResultsError($"No search results for \"{parsedTitle}\" ");
+            }
+            else if (resultCount == 1)
+            {
+                result = searchResult.First();
+            }
+            else if (parsedReleaseDate == null)
+            {
+                throw new MultipleSearchResultsError($"Multiple search results for \"{parsedTitle}\"; count = {resultCount}");
+            }
+            else
+            {
+                List<MovieSearchResult> searchResultFiltered = searchResult
+                    .Where(r => r.ReleaseDate.ToString() == parsedReleaseDate.Trim())
+                    .ToList();
+                int resultCountFiltered = searchResultFiltered.Count();
+
+                if (resultCountFiltered == 0)
+                {
+                    throw new NoSearchResultsError(
+                        $"No search results for \"{parsedTitle}\" with release date = {parsedReleaseDate}");
+                }
+                else if (resultCountFiltered > 1)
+                {
+                    throw new MultipleSearchResultsError(
+                        $"Multiple search results for \"{parsedTitle}\"  with release date = {parsedReleaseDate}; count = {resultCount}");
+                }
+                result = searchResultFiltered.First();
+            }
+
+            return new Movie() {
+                Title = result.Title,
+                ExternalId = result.ExternalId,
+                OriginalTitle = result.OriginalTitle,
+                ReleaseDate = result.ReleaseDate
+                };
+        }
+
+        private static IEnumerable<string> SplitTitleIntoTokens(string parsedTitle)
+        {
+            var movieTitle = parsedTitle.Trim().ToLower();
+            char[] punctuation = movieTitle.Where(Char.IsPunctuation).Distinct().ToArray();
+            return movieTitle.Split().Select(s => s.Trim(punctuation));
+        }
+    }
+
+}
