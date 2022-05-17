eec988ff73a714f3e8a68f83b24beffd5175ffc1
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-17 18:13:25 +0100

    testclass setup

--- DepotTests/CRUDTests/RipToMovieLinkerTests.cs
+++ DepotTests/CRUDTests/RipToMovieLinkerTests.cs
@@ -1,7 +1,53 @@
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+using FilmCRUD;
+using FilmDomain.Interfaces;
+using ConfigUtils.Interfaces;
+using MovieAPIClients.Interfaces;
+
 namespace DepotTests.CRUDTests
 {
     public class RipToMovieLinkerTests
     {
+        private readonly Mock<IMovieRipRepository> _movieRipRepositoryMock;
 
+        private readonly Mock<IMovieRepository> _movieRepositoryMock;
+
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        private readonly Mock<IMovieAPIClient> _movieAPIClientMock;
+
+        private readonly Mock<IAppSettingsManager> _appSettingsManagerMock;
+
+        private readonly RipToMovieLinker _ripToMovieLinker;
+
+        public RipToMovieLinkerTests()
+        {
+            this._movieAPIClientMock = new Mock<IMovieAPIClient>();
+            this._appSettingsManagerMock = new Mock<IAppSettingsManager>();
+
+            this._movieRipRepositoryMock = new Mock<IMovieRipRepository>();
+            this._movieRepositoryMock = new Mock<IMovieRepository>();
+
+            this._unitOfWorkMock = new Mock<IUnitOfWork>();
+            this._unitOfWorkMock
+                .SetupGet(u => u.MovieRips)
+                .Returns(this._movieRipRepositoryMock.Object);
+            this._unitOfWorkMock
+                .SetupGet(u => u.Movies)
+                .Returns(this._movieRepositoryMock.Object);
+
+            this._ripToMovieLinker = new RipToMovieLinker(
+                this._unitOfWorkMock.Object,
+                this._appSettingsManagerMock.Object,
+                this._movieAPIClientMock.Object);
+        }
+
+        [Fact]
+        public void TestMethod()
+        {
+        }
     }
 }
--- FilmCRUD/RipToMovieLinker.cs
+++ FilmCRUD/RipToMovieLinker.cs
@@ -1,7 +1,24 @@
+using ConfigUtils.Interfaces;
+using FilmCRUD.Helpers;
+using FilmDomain.Interfaces;
+using MovieAPIClients.Interfaces;
+
 namespace FilmCRUD
 {
     public class RipToMovieLinker
     {
+        private IUnitOfWork _unitOfWork { get; init; }
+
+        private IAppSettingsManager _appSettingsManager { get; init; }
+
+        private MovieFinder _movieFinder { get; init; }
+
+        public RipToMovieLinker(IUnitOfWork unitOfWork, IAppSettingsManager appSettingsManager, IMovieAPIClient movieAPIClient)
+        {
+            this._movieFinder = new MovieFinder(movieAPIClient);
+            this._unitOfWork = unitOfWork;
+            this._appSettingsManager = appSettingsManager;
+        }
     }
 
 }
