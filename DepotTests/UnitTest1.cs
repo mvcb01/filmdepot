4655d6cb72c6049b8c92d6e0f40a769e59f8cba8
mvcb01 <miguelvcbarros01@gmail.com>
2022-05-23 19:28:09 +0100

    deleted unused test content

--- DepotTests/UnitTest1.cs
+++ DepotTests/UnitTest1.cs
@@ -12,17 +12,6 @@ namespace DepotTests
         public async void Test1()
         {
 
-            var appSettingsManager = new AppSettingsManager();
-            string apiKey = appSettingsManager.GetApiKey("TheMovieDb");
-            var apiCaller = new TheMovieDbAPIClient(apiKey);
-            {}
-
-            // var result = await apiCaller.SearchMovieAsync("the tenant");
-            // var result = await apiCaller.GetMovieGenresAsync(11482);
-            var result = await apiCaller.GetMovieActorsAsync(11482);
-
-            var resultList = result.ToList();
-            {}
         }
     }
 }
