ecc9ea4ccead0f2c2691a4cda563b79ef19f4a08
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-01 20:16:25 +0100

    LinkOptions: new options

--- FilmCRUD/Verbs/LinkOptions.cs
+++ FilmCRUD/Verbs/LinkOptions.cs
@@ -7,6 +7,17 @@ namespace FilmCRUD.Verbs
     public class LinkOptions
     {
 
+        [Option(SetName = "SearchLocallyAndOnline", HelpText = "Procura localmente e online")]
+        public bool Search { get; set; }
+
+        [Option(SetName = "LinkFromManualExternalIds", HelpText = "Procura online a partir dos external ids manuais")]
+        public bool FromManualExtIds { get; set; }
+
+        [Option(SetName = "GetUnlinkedMovieRips", HelpText = "nomes dos movierips não linkados")]
+        public bool GetUnlinkedRips { get; set; }
+
+        [Option(SetName = "ValidateManualExternalIds", HelpText = "valida os external ids manuais")]
+        public bool ValidateManualExtIds { get; set; }
 
     }
 
