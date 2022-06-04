5a89e89fc2b118f274aaa99bec8a4a86c7f4b676
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-04 16:36:06 +0100

    new migration RemovedExternalIdFromGenre

---
+++ FilmDataAccess.EFCore/Migrations/20220604153552_RemovedExternalIdFromGenre.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmDataAccess.EFCore.Migrations
+{
+    public partial class RemovedExternalIdFromGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Genres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalId",
+                table: "Genres",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
