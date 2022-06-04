d90cbce09209e688d9cac2f6bc7c7b46c32878ee
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-04 16:30:59 +0100

    new migration AddedIMDBIdMovieProperty

---
+++ FilmDataAccess.EFCore/Migrations/20220604153038_AddedIMDBIdMovieProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmDataAccess.EFCore.Migrations
+{
+    public partial class AddedIMDBIdMovieProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IMDBId",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IMDBId",
+                table: "Movies");
+        }
+    }
+}
