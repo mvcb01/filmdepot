c2e6959ed2d3aba773e6079c8403d5fdaff846bf
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-10 19:15:45 +0100

    new step in entitytypeconfig and new migration

---
+++ FilmDataAccess.EFCore/Migrations/20220610180422_AddedKeywordsMovieProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmDataAccess.EFCore.Migrations
+{
+    public partial class AddedKeywordsMovieProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Keywords",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Keywords",
+                table: "Movies");
+        }
+    }
+}
--- FilmDataAccess.EFCore/ModelConfiguration/MovieEntityTypeConfiguration.cs
+++ FilmDataAccess.EFCore/ModelConfiguration/MovieEntityTypeConfiguration.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using FilmDomain.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.Json;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq;
+using System;
 
 namespace FilmDataAccess.EFCore.ModelConfiguration
 {
@@ -12,6 +17,14 @@ namespace FilmDataAccess.EFCore.ModelConfiguration
 
             // a combinação (título, título original, release date) deve identificar bem um filme
             builder.HasAlternateKey(m => new { m.Title, m.OriginalTitle, m.ReleaseDate });
+
+            // see:
+            //  https://docs.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=fluent-api
+            builder
+                .Property(m => m.Keywords)
+                .HasConversion(
+                    k => JsonSerializer.Serialize(k, (JsonSerializerOptions)null),
+                    k => JsonSerializer.Deserialize<ICollection<string>>(k, (JsonSerializerOptions)null));
 
             builder.Navigation(m => m.Actors).AutoInclude();
             builder.Navigation(m => m.Directors).AutoInclude();
