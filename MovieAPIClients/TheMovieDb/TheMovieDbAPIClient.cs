3a8f556fb41e0087d728f1a8abe6fd8f1643334d
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-13 16:42:45 +0100

    temp using directive

--- MovieAPIClients/TheMovieDb/TheMovieDbAPIClient.cs
+++ MovieAPIClients/TheMovieDb/TheMovieDbAPIClient.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.Text.Json;
 
+using MovieAPIClients.Extensions;
 using MovieAPIClients.Interfaces;
 
 namespace MovieAPIClients.TheMovieDb
