2e894866088c381054ce72c0391e3dcbb34a9833
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-26 20:28:39 +0100

    new class and tests class: MovieDetailsFetcherActors

---
+++ DepotTests/CRUDTests/MovieDetailsFetcherActorsTests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+using FilmCRUD;
+using FilmDomain.Entities;
+using FilmDomain.Interfaces;
+using MovieAPIClients;
+using MovieAPIClients.Interfaces;
+
+namespace DepotTests.CRUDTests
+{
+    public class MovieDetailsFetcherActorsTests
+    {
+
+    }
+}
