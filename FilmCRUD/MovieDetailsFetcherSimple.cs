d43fe3b4d6adb652894f7209b734b12497cb2fb3
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-30 19:27:26 +0100

    documenting MovieDetailsFetcherAbstract

---
+++ FilmCRUD/MovieDetailsFetcherSimple.cs
@@ -0,0 +1,9 @@
+namespace FilmCRUD
+{
+    /// <summary>
+    /// Class <c>Point</c> models a point in a two-dimensional plane.
+    /// </summary>
+    public class MovieDetailsFetcherSimple
+    {
+    }
+}
