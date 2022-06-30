026f10767f51ea2d66927549291d50605a4bfa8d
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-30 21:29:24 +0100

    MovieDetailsFetcherSimpleTests: third failing unittest

--- DepotTests/CRUDTests/MovieDetailsFetcherSimpleTests.cs
+++ DepotTests/CRUDTests/MovieDetailsFetcherSimpleTests.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Moq;
 using Xunit;
 using FluentAssertions;
+using FluentAssertions.Execution;
 
 using FilmCRUD;
 using FilmDomain.Entities;
@@ -70,6 +71,45 @@ namespace DepotTests.CRUDTests
             this._movieAPIClientMock.Verify(
                 m => m.GetMovieKeywordsAsync(It.Is<int>( i => i == firstExternalId | i == secondExternalId)),
                 Times.Exactly(2));
+        }
+
+        [Fact]
+        public async void GetKeywordsForMovies_WithMoviesMissingKeywords_ShouldPopulateKeywordsCorrectly()
+        {
+            // arrange
+            int firstExternalId = 101;
+            int secondExternalId = 102;
+            var firstMovieWithoutKeywords = new Movie() {
+                Title = "total recall",
+                ReleaseDate = 1989,
+                ExternalId = firstExternalId };
+            var secondMovieWithoutKeywords = new Movie() {
+                Title = "get carter",
+                ReleaseDate = 1971,
+                ExternalId = secondExternalId };
+
+            var firstMovieKeywords = new string[] { "planet mars", "oxygen"};
+            var secondMovieKeywords = new string[] { "hitman", "northern england", "loss of loved one"};
+            this._movieRepositoryMock
+                .Setup(m => m.GetMoviesWithoutKeywords())
+                .Returns(new Movie[] { firstMovieWithoutKeywords, secondMovieWithoutKeywords });
+
+            this._movieAPIClientMock
+                .Setup(cl => cl.GetMovieKeywordsAsync(firstExternalId))
+                .ReturnsAsync(firstMovieKeywords);
+            this._movieAPIClientMock
+                .Setup(cl => cl.GetMovieKeywordsAsync(secondExternalId))
+                .ReturnsAsync(secondMovieKeywords);
+
+            // act
+            await this._movieDetailsFetcherSimple.PopulateMovieKeywords();
+
+            // assert
+            using (new AssertionScope())
+            {
+                firstMovieWithoutKeywords.Keywords.Should().BeEquivalentTo(firstMovieKeywords);
+                secondMovieWithoutKeywords.Keywords.Should().BeEquivalentTo(secondMovieKeywords);
+            }
         }
 
 
