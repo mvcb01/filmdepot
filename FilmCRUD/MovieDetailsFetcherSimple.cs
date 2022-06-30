2c42b1c8bee38edc6f02933b4438fdae46e1d423
mvcb01 <miguelvcbarros01@gmail.com>
2022-06-30 19:36:40 +0100

    MovieDetailsFetcherSimple: class sketch and tests

--- FilmCRUD/MovieDetailsFetcherSimple.cs
+++ FilmCRUD/MovieDetailsFetcherSimple.cs
@@ -1,9 +1,25 @@
+using FilmDomain.Interfaces;
+using MovieAPIClients.Interfaces;
+
 namespace FilmCRUD
 {
     /// <summary>
-    /// Class <c>Point</c> models a point in a two-dimensional plane.
+    /// Class <c>MovieDetailsFetcherSimple</c> To populate Movie properties that do not have a custom return type
+    /// on the relevant method of IMovieAPIClient.
+    /// Example: the Keywords movie property, which is a IEnumerable<string> found by the
+    /// method IMovieAPIClient.GetMovieKeywordsAsync
     /// </summary>
     public class MovieDetailsFetcherSimple
     {
+        private IUnitOfWork _unitOfWork { get; init; }
+
+        private IMovieAPIClient _movieAPIClient { get; init; }
+
+        public MovieDetailsFetcherSimple(IUnitOfWork unitOfWork, IMovieAPIClient movieAPIClient)
+        {
+            this._unitOfWork = unitOfWork;
+            this._movieAPIClient = movieAPIClient;
+        }
     }
+
 }
