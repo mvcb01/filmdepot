9710b5fb47e6e142747973691fabe7dd2329bc0c
mvcb01 <miguelvcbarros01@gmail.com>
2022-07-02 16:53:34 +0100

    MovieDetailsFetcherSimple: populating movie keywords

--- FilmCRUD/MovieDetailsFetcherSimple.cs
+++ FilmCRUD/MovieDetailsFetcherSimple.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 using FilmDomain.Entities;
 using FilmDomain.Interfaces;
@@ -28,7 +29,19 @@ namespace FilmCRUD
         public async Task PopulateMovieKeywords()
         {
             IEnumerable<Movie> moviesWithoutKeywords = this._unitOfWork.Movies.GetMoviesWithoutKeywords();
-            {}
+
+            var keywordTasks = new Dictionary<int, Task<IEnumerable<string>>>();
+            foreach (var movie in moviesWithoutKeywords)
+            {
+                keywordTasks.Add(movie.ExternalId, this._movieAPIClient.GetMovieKeywordsAsync(movie.ExternalId));
+            }
+
+            await Task.WhenAll(keywordTasks.Values);
+
+            foreach (var movie in moviesWithoutKeywords)
+            {
+                movie.Keywords = keywordTasks[movie.ExternalId].Result.ToList();
+            }
         }
     }
 
