fef0689d58caba8ea6f4f51b9213fcca0979e081
mvcb01 <miguelvcbarros01@gmail.com>
2022-07-03 14:52:12 +0100

    visit option persistcontents is now a string

--- FilmCRUD/Verbs/VisitOptions.cs
+++ FilmCRUD/Verbs/VisitOptions.cs
@@ -4,7 +4,7 @@ namespace FilmCRUD.Verbs
 {
     // Exemplos:
     //  dotnet run -- visit --listcontents
-    //  dotnet run -- visit --persistcontents --contentsdate 20220306
+    //  dotnet run -- visit --persistcontents 20220306
     [Verb("visit", HelpText = "Para gerar o txt com os conteúdos da warehouse e persistir no repo")]
     public class VisitOptions
     {
@@ -12,10 +12,7 @@ namespace FilmCRUD.Verbs
         public bool ListContents { get; set; }
 
         [Option(SetName = "PersistingContents", HelpText = "Lê conteúdos da warehouse de um txt e persiste no repo")]
-        public bool PersistContents { get; set; }
-
-        [Option(SetName = "PersistingContents", HelpText = "Sufixo do txt a ler, por exemplo 20210923")]
-        public string ContentsDate { get; set; }
+        public string PersistContents { get; set; }
     }
 
 }
