efe2d81ce40ba2c60f9d93cff5e11962674ca8b8
mvcb01 <miguelvcbarros01@gmail.com>
2022-07-03 14:59:01 +0100

    converted cli helptexts to ENG

--- FilmCRUD/Verbs/ScanRipsOptions.cs
+++ FilmCRUD/Verbs/ScanRipsOptions.cs
@@ -2,22 +2,22 @@ using CommandLine;
 
 namespace FilmCRUD.Verbs
 {
-    // Exemplos:
+    // Examples:
     //  dotnet run -- scan --countbyreleasedate
     //  dotnet run -- scan --withreleasedate 2011
-    [Verb("scanrips", HelpText = "Informação dos rips existentes")]
+    [Verb("scanrips", HelpText = "info from existing movie rips")]
     public class ScanRipsOptions
     {
-        [Option(SetName = "CountRipsByReleaseDate", HelpText = "Contagem por release date")]
+        [Option(SetName = "CountRipsByReleaseDate", HelpText = "rip count by release date for latest visit")]
         public bool CountByReleaseDate { get; set; }
 
-        [Option(SetName = "GetRipsByReleaseDate", HelpText = "Rips por release date")]
+        [Option(SetName = "GetRipsByReleaseDate", HelpText = "get all rips from last visit with releasedate YYYYMMDD")]
         public string WithReleaseDate { get; set; }
 
-        [Option(SetName = "CountRipsByVisit", HelpText = "Contagem por visita")]
+        [Option(SetName = "CountRipsByVisit", HelpText = "rip count by visit")]
         public bool CountByVisit { get; set; }
 
-        [Option(SetName = "LastVisitRipDifference", HelpText = "Novos rips e rips removidos na última visita")]
+        [Option(SetName = "LastVisitRipDifference", HelpText = "movie rip difference from last two visits: added and removed")]
         public bool LastVisitDiff { get; set; }
 
     }
