a35c66419071dcb57d2653d29556f69d3c7db178
mvcb01 <miguelvcbarros01@gmail.com>
2022-07-05 19:08:14 +0100

    setting up ScanMoviesManagerTests

--- DepotTests/CRUDTests/ScanMoviesManagerTests.cs
+++ DepotTests/CRUDTests/ScanMoviesManagerTests.cs
@@ -1,5 +1,47 @@
+using Xunit;
+using FluentAssertions;
+using Moq;
+
+using FilmDomain.Entities;
+using FilmDomain.Interfaces;
+
 namespace DepotTests.CRUDTests
 {
     public class ScanMoviesManagerTests
-    {}
+    {
+
+
+        private readonly Mock<IActorRepository> _actoRepositoryMock;
+
+        private readonly Mock<IGenreRepository> _genreRepositoryMock;
+
+        private readonly Mock<IDirectorRepository> _directorRepositoryMock;
+
+        private readonly Mock<IMovieRepository> _movieRepositoryMock;
+
+
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        public ScanMoviesManagerTests()
+        {
+            this._actoRepositoryMock = new Mock<IActorRepository>(MockBehavior.Strict);
+            this._genreRepositoryMock = new Mock<IGenreRepository>(MockBehavior.Strict);
+            this._directorRepositoryMock = new Mock<IDirectorRepository>(MockBehavior.Strict);
+            this._movieRepositoryMock = new Mock<IMovieRepository>(MockBehavior.Strict);
+
+            this._unitOfWorkMock = new Mock<IUnitOfWork>();
+            this._unitOfWorkMock
+                .SetupGet(u => u.Actors)
+                .Returns(this._actoRepositoryMock.Object);
+            this._unitOfWorkMock
+                .SetupGet(u => u.Genres)
+                .Returns(this._genreRepositoryMock.Object);
+            this._unitOfWorkMock
+                .SetupGet(u => u.Directors)
+                .Returns(this._directorRepositoryMock.Object);
+            this._unitOfWorkMock
+                .SetupGet(u => u.Movies)
+                .Returns(this._movieRepositoryMock.Object);
+        }
+    }
 }
