6c44f06579035215617d9af5e4b552e2c1b315a1
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-07 14:41:24 +0100

    added imdb id to prettyformat

--- FilmDomain/Extensions/EntityExtensions.cs
+++ FilmDomain/Extensions/EntityExtensions.cs
@@ -115,7 +115,7 @@ namespace FilmDomain.Extensions
         {
             string _genres = string.Join(" | ", movie.Genres.Select(g => g.Name));
             string _directors = string.Join(", ", movie.Directors.Select(d => d.Name));
-            return string.Join('\n', new string[] {movie.ToString(), _genres, $"Directors: {_directors}"});
+            return string.Join('\n', new string[] {movie.ToString(), _genres, $"Directors: {_directors}", $"IMDB id: {movie.IMDBId}"});
         }
     }
 }
