efc38b227daee144d69fea2ccfbee7e9f0692976
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-09 20:11:09 +0100

    local deployment script

--- _DeploymentScripts/1_DeployLocally.ps1
+++ _DeploymentScripts/1_DeployLocally.ps1
@@ -1,1 +1,16 @@
-echo $env:USERPROFILE
+
+$cwd = (Get-Item .).FullName
+
+$user = $env:USERPROFILE
+$deploymentDir = $user + '\entertainmentdepot_PROD'
+
+$solutionRoot = (get-item $PSScriptRoot).parent.FullName
+
+echo "-----------"
+echo ("Deploying to {0}" -f $deploymentDir)
+echo " "
+
+cd $solutionRoot
+dotnet publish .\entertainmentdepot.sln -o $deploymentDir -c Release
+cd $cwd
+
