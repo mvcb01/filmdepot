be7db3c78e6fe49cb75f0491907af322ad2e55d7
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-09 22:30:52 +0100

    db created if not exists with all migrations executed

--- _DeploymentScripts/3_RunMigrationScripts.ps1
+++ _DeploymentScripts/3_RunMigrationScripts.ps1
@@ -5,6 +5,36 @@ $deploymentDir = Join-Path -Path $user -ChildPath 'entertainmentdepot_PROD'
 
 cd $deploymentDir
 
-mkdir .\backups
+# creates the backup dir if not exists; no need to print anything to the console
+$backups = Join-Path -Path $deploymentDir -ChildPath 'backups'
+[System.IO.Directory]::CreateDirectory($backups) | out-null
+
+$dbExists = Test-Path -Path FilmDb.db -PathType Leaf
+
+if ($dbExists)
+{
+    # TODO: find all the migration scripts not executed yet using table __EFMigrationsHistory
+    echo "db exists..."
+}
+else
+{
+    echo "Database FilmDb.db does not exist, creating..."
+    New-Item -Path FilmDb.db -ItemType File | out-null
+
+    echo "Running all migrations:"
+    $dirContentsString = ls .\Migrations
+    $dirContents = $dirContentsString -split [Environment]::NewLine
+    foreach ($file in $dirContents)
+    {
+        if ($file -match '^[0-9]{14}_' -And  $file -match '.sql$')
+        {
+            $scriptPath = Join-Path -Path $deploymentDir -ChildPath 'Migrations' | Join-Path -ChildPath $file
+            echo $file
+            Get-Content $scriptPath -Raw | sqlite3 .\FilmDb.db
+        }
+    }
+
+}
+
 
 cd $cwd
