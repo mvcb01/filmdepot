1a4206b8bbccb2d2df25a15fa76ea8f64b9e214f
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-09 21:10:34 +0100

    migratins list output is parsed

--- _DeploymentScripts/2_GenerateMigrationScripts.ps1
+++ _DeploymentScripts/2_GenerateMigrationScripts.ps1
@@ -2,3 +2,20 @@ $cwd = (Get-Item .).FullName
 
 $user = $env:USERPROFILE
 $deploymentDir = $user + '\entertainmentdepot_PROD'
+
+$solutionRoot = (get-item $PSScriptRoot).parent.FullName
+
+cd ($solutionRoot + '\FilmDataAccess.EFCore')
+
+$outputString = dotnet ef migrations list
+$lines = $outputString -split [Environment]::NewLine
+foreach ($line in $lines)
+{
+  # migrations have names like 20220510145828_DummyMigration
+  if ($line -match '^[0-9]{14}_')
+  {
+    echo $line
+  }
+}
+
+cd $cwd
