60685b89d2321a270399fa99d8771b3e5cb07725
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-09 22:10:03 +0100

    run migrations: dummy script

---
+++ _DeploymentScripts/3_RunMigrationScripts.ps1
@@ -0,0 +1,10 @@
+$cwd = (Get-Item .).FullName
+
+$user = $env:USERPROFILE
+$deploymentDir = Join-Path -Path $user -ChildPath 'entertainmentdepot_PROD'
+
+cd $deploymentDir
+
+mkdir .\backups
+
+cd $cwd
