64553f6dc1214809040aa70ef5e93b9abf0fe200
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-11 18:46:35 +0100

    run migrations script ready

--- _DeploymentScripts/2_GenerateMigrationScripts.ps1
+++ _DeploymentScripts/2_GenerateMigrationScripts.ps1
@@ -7,19 +7,35 @@ $solutionRoot = (get-item $PSScriptRoot).parent.FullName
 
 cd ($solutionRoot + '\FilmDataAccess.EFCore')
 
+# creates the Migration dir if not exists; no need to print anything to the console
+$migrationsDir = Join-Path -Path $deploymentDir -ChildPath 'Migrations'
+[System.IO.Directory]::CreateDirectory($migrationsDir) | out-null
+
 $outputString = dotnet ef migrations list
 $lines = $outputString -split [Environment]::NewLine
+$existingMigrations = Get-ChildItem -Path $migrationsDir | Select -ExpandProperty Name
 $migrationFrom = '0'
 $migrationTo = ''
+
 foreach ($line in $lines)
 {
   # migrations have names like 20220510145828_SomeMigrationName
   if ($line -match '^[0-9]{14}_')
   {
     $migrationTo = $line.Trim()
-    $filePath = Join-Path -Path $deploymentDir -ChildPath 'Migrations' | Join-Path -ChildPath ($migrationTo + ".sql")
-    echo ("Generating " + $filePath)
-    dotnet ef migrations script $migrationFrom $migrationTo -o $filePath
+    $filename = $migrationTo + ".sql"
+    $filePath = Join-Path -Path $migrationsDir -ChildPath $filename
+
+    if ($existingMigrations -Contains $filename)
+    {
+      echo ("Migration already exists: " + $filePath)
+    }
+    else
+    {
+      echo ("Generating " + $filePath)
+      dotnet ef migrations script $migrationFrom $migrationTo -o $filePath
+    }
+
     $migrationFrom = $migrationTo
   }
 }
--- _DeploymentScripts/3_RunMigrationScripts.ps1
+++ _DeploymentScripts/3_RunMigrationScripts.ps1
@@ -5,37 +5,63 @@ $deploymentDir = Join-Path -Path $user -ChildPath 'entertainmentdepot_PROD'
 
 cd $deploymentDir
 
-# creates the backup dir if not exists; no need to print anything to the console
-$backups = Join-Path -Path $deploymentDir -ChildPath 'backups'
-[System.IO.Directory]::CreateDirectory($backups) | out-null
+$migrationsDir = Join-Path -Path $deploymentDir -ChildPath 'Migrations'
+$migrationsInDir = Get-ChildItem -Path $migrationsDir | Select -ExpandProperty Name
+$migrationsToRun = @()
 
 $dbExists = Test-Path -Path FilmDb.db -PathType Leaf
-
 if ($dbExists)
 {
-    # TODO: backup the database, find all the migration scripts not executed yet using table __EFMigrationsHistory and
-    # execute one by one; any error and the original db should be restored
-    echo "db exists..."
+    $migrationsHistory = sqlite3 .\FilmDb.db "select MigrationId from __EFMigrationsHistory order by MigrationId desc"
+
+    foreach ($file in $migrationsInDir)
+    {
+        $isMigrationScript = $file -match '^[0-9]{14}_' -And $file -match '.sql$'
+        if (-not $isMigrationScript) { continue }
+
+        $migrationName = [io.path]::GetFileNameWithoutExtension($file)
+        if ($migrationsHistory -Contains $migrationName)
+        {
+            echo ("Migration already ran: " + $file)
+        }
+        else
+        {
+            $fullPath = Join-Path -Path $migrationsDir -ChildPath $file
+            $migrationsToRun += $fullPath
+        }
+    }
 }
 else
 {
     echo "Database FilmDb.db does not exist, creating..."
     New-Item -Path FilmDb.db -ItemType File | out-null
 
-    echo "Running all migrations:"
-    $dirContentsString = ls .\Migrations
-    $dirContents = $dirContentsString -split [Environment]::NewLine
-    foreach ($file in $dirContents)
+    foreach ($file in $migrationsInDir)
     {
-        if ($file -match '^[0-9]{14}_' -And $file -match '.sql$')
-        {
-            $scriptPath = Join-Path -Path $deploymentDir -ChildPath 'Migrations' | Join-Path -ChildPath $file
-            echo $file
-            Get-Content $scriptPath -Raw | sqlite3 .\FilmDb.db
-        }
-    }
+        $isMigrationScript = $file -match '^[0-9]{14}_' -And $file -match '.sql$'
+        if (-not $isMigrationScript) { continue }
 
+        $fullPath = Join-Path -Path $migrationsDir -ChildPath $file
+        $migrationsToRun += $fullPath
+    }
 }
 
+if ($migrationsToRun.Count -ne 0)
+{
+    # creates the backup dir if not exists; no need to print anything to the console
+    $backups = Join-Path -Path $deploymentDir -ChildPath 'backups'
+    [System.IO.Directory]::CreateDirectory($backups) | out-null
+
+    $dateStr = '{0:yyyyMMdd}' -f $(Get-Date -f "yyyyMMddTHHmmss")
+    $dbBackup = ".\backups\FilmDb_{0}.db" -f $dateStr
+    echo ("Backing up .\FilmDb --> {0} " -f $dbBackup)
+    Copy-Item .\FilmDb.db -Destination $dbBackup
+
+    foreach ($migrationPath in $migrationsToRun)
+    {
+        echo ("Running migration: " + $migrationPath)
+        Get-Content $migrationPath -Raw | sqlite3 .\FilmDb.db
+    }
+}
 
 cd $cwd
