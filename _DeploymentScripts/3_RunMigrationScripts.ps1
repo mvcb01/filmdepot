a38da69143436ccf30260fc37ef8c70d69c34fbe
mvcb01 <105233935+mvcb01@users.noreply.github.com>
2022-08-15 17:56:59 +0100

    Deployment scripts: abort if not in main branch (#54)
    * whitespace
    
    * all ps1 scripts amended

--- _DeploymentScripts/3_RunMigrationScripts.ps1
+++ _DeploymentScripts/3_RunMigrationScripts.ps1
@@ -1,3 +1,12 @@
+$currentBranchName = git branch --show-current
+if ($currentBranchName -ne "main" -And $currentBranchName -ne "master")
+{
+    echo ("Current branch not main or master: " + $currentBranchName)
+    exit
+}
+
+# ------
+
 $cwd = (Get-Item .).FullName
 
 $user = $env:USERPROFILE
