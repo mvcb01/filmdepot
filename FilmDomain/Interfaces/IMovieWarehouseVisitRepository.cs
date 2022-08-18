7b6685e4a5319e84f8005d2f1b39752c3f5b0f76
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-18 21:52:11 +0100

    formattting

--- FilmDomain/Interfaces/IMovieWarehouseVisitRepository.cs
+++ FilmDomain/Interfaces/IMovieWarehouseVisitRepository.cs
@@ -2,18 +2,15 @@ using System;
 using System.Collections.Generic;
 using FilmDomain.Entities;
 
-
 namespace FilmDomain.Interfaces
 {
     public interface IMovieWarehouseVisitRepository : IEntityRepository<MovieWarehouseVisit>
     {
-
         MovieWarehouseVisit GetClosestMovieWarehouseVisit();
 
         MovieWarehouseVisit GetClosestMovieWarehouseVisit(DateTime dt);
 
         MovieWarehouseVisit GetPreviousMovieWarehouseVisit(MovieWarehouseVisit visit);
-
     }
 
 }
