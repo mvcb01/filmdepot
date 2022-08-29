1308a576ac48218d9f7e7bb405b74d8d5a21ff36
mvcb01 <miguelvcbarros01@gmail.com>
2022-08-29 20:28:49 +0100

    new option in ScanMoviesOptions verb: VisitDiff

--- FilmCRUD/Verbs/ScanMoviesOptions.cs
+++ FilmCRUD/Verbs/ScanMoviesOptions.cs
@@ -30,6 +30,10 @@ namespace FilmCRUD.Verbs
         [Option(SetName = "ByDirectorOptions", HelpText = "get descending movie count by director")]
         public bool ByDirector { get; set; }
 
+        [Option(SetName = "VisitMovieDifference", Separator = ':',
+            HelpText = "movie difference between two visits with dates YYYYMMDD: added and removed movie rips; example: 20100101:20100102")]
+        public IEnumerable<string> VisitDiff { get; set; }
+
         [Option(SetName = "ListVisitsOption", HelpText = "helper; list dates for all visits")]
         public bool ListVisits { get; set; }
 
