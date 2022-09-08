a656473e33adb6846d99634622a9db79fdfd9736
mvcb01 <miguelvcbarros01@gmail.com>
2022-09-08 20:52:06 +0100

    new interface: IRetryPolicyConfig

---
+++ ConfigUtils/Interfaces/IRetryPolicyConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigUtils.Interfaces
+{
+    public interface IRetryPolicyConfig
+    {
+        int RetryCount { get; set; }
+
+        TimeSpan SleepDuration { get; set; }
+    }
+}
