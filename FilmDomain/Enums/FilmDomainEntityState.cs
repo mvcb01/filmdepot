e591d0fbbf6b4235e3435796d52e623a78514f02
mvcb01 <miguelvcbarros01@gmail.com>
2022-09-11 15:45:27 +0100

    enum members summary

--- FilmDomain/Enums/FilmDomainEntityState.cs
+++ FilmDomain/Enums/FilmDomainEntityState.cs
@@ -6,22 +6,32 @@ namespace FilmDomain.Enums
     /// </summary>
     public enum FilmDomainEntityState
     {
-        // The entity is not being tracked by the context.
+        /// <summary>
+        ///     The entity is not being tracked by the context.
+        /// </summary>
         Detached = 0,
 
-        //The entity is being tracked by the context and exists in the database.
-        //Its property values have not changed from the values in the database.
+        /// <summary>
+        ///     The entity is being tracked by the context and exists in the database. Its property
+        ///     values have not changed from the values in the database.
+        /// </summary>
         Unchanged = 1,
 
-        // The entity is being tracked by the context and exists in the database.
-        // It has been marked for deletion from the database.
+        /// <summary>
+        ///     The entity is being tracked by the context and exists in the database. It has been marked
+        ///     for deletion from the database.
+        /// </summary>
         Deleted = 2,
 
-        // The entity is being tracked by the context and exists in the database.
-        // Some or all of its property values have been modified.
+        /// <summary>
+        ///     The entity is being tracked by the context and exists in the database. Some or all of its
+        ///     property values have been modified.
+        /// </summary>
         Modified = 3,
 
-        // The entity is being tracked by the context but does not yet exist in the database.
+        /// <summary>
+        ///     The entity is being tracked by the context but does not yet exist in the database.
+        /// </summary>
         Added = 4
     }
 }
