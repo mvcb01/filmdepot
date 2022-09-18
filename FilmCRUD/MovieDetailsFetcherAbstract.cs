7b8bdb5611bc49589eddf8612dc3b4a503dd73d8
mvcb01 <miguelvcbarros01@gmail.com>
2022-09-18 13:00:31 +0100

    PopulateDetails: deleted old method

--- FilmCRUD/MovieDetailsFetcherAbstract.cs
+++ FilmCRUD/MovieDetailsFetcherAbstract.cs
@@ -144,53 +144,6 @@ namespace FilmCRUD
             this._fileSystemIOWrapper.WriteAllText(errorsFpath, string.Join("\n\n", errors));
         }
 
-        public async Task PopulateDetails_OLD()
-        {
-            IEnumerable<Movie> moviesWithoutDetails = GetMoviesWithoutDetails();
-            if (!moviesWithoutDetails.Any())
-            {
-                return;
-            }
-
-            // maps each movie id to the task that returns its details from the movie api
-            var detailTasks = new Dictionary<int, Task<IEnumerable<TAPIResult>>>();
-            foreach (Movie movie in moviesWithoutDetails)
-            {
-                detailTasks.Add(movie.ExternalId, GetMovieDetailsFromApiAsync(movie.ExternalId));
-            }
-
-            await Task.WhenAll(detailTasks.Values);
-
-            // gets the IEnumerable<TAPIResult> of each task, flattens to a single IEnumerable<TAPIResult> and
-            // gets the distinct results using the ExternalId property
-            IEnumerable<TAPIResult> distinctDetailResults = detailTasks.Values
-                .SelectMany(t => t.Result)
-                .GroupBy(res => res.ExternalId)
-                .Select(group => group.First());
-
-            IEnumerable<TDetailEntity> existingDetailEntities = GetExistingDetailEntitiesInRepo();
-            IEnumerable<int> existingDetailEntitiesExtIds = existingDetailEntities.Select(d => d.ExternalId);
-
-            // new detail entities: these are still not part of its repo;
-            // the ToList method is called to force execution and make sure such entities are
-            // created exactly once;
-            IEnumerable<TDetailEntity> newDetailEntities = distinctDetailResults
-                .Where(res => !existingDetailEntitiesExtIds.Contains(res.ExternalId))
-                .Select(res => CastApiResultToDetailEntity(res))
-                .ToList();
-
-            // gets the new details for each movie
-            foreach (Movie movie in moviesWithoutDetails)
-            {
-                IEnumerable<int> detailExternalIdsForMovie = detailTasks[movie.ExternalId].Result.Select(res => res.ExternalId);
-                IEnumerable<TDetailEntity> movieDetailsInRepo = existingDetailEntities.Where(d => detailExternalIdsForMovie.Contains(d.ExternalId));
-                IEnumerable<TDetailEntity> movieDetailsNew = newDetailEntities.Where(d => detailExternalIdsForMovie.Contains(d.ExternalId));
-                AddDetailsToMovieEntity(movie, movieDetailsInRepo.Concat(movieDetailsNew));
-            }
-
-            this._unitOfWork.Complete();
-        }
-
     }
 
 }
