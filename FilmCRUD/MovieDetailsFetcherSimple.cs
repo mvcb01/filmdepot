67861672a32ad99486946703618deec5a99f6447
mvcb01 <miguelvcbarros01@gmail.com>
2022-09-18 15:04:31 +0100

    PopulateMovieKeywords amended to use policies

--- FilmCRUD/MovieDetailsFetcherSimple.cs
+++ FilmCRUD/MovieDetailsFetcherSimple.cs
@@ -7,6 +7,15 @@ using FilmDomain.Interfaces;
 using MovieAPIClients.Interfaces;
 using ConfigUtils.Interfaces;
 using FilmCRUD.Interfaces;
+using FilmCRUD.Helpers;
+using Polly.RateLimit;
+using Polly.Wrap;
+using Polly;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Collections;
+using System.IO;
 
 namespace FilmCRUD
 {
@@ -39,6 +48,44 @@ namespace FilmCRUD
         }
 
         public async Task PopulateMovieKeywords()
+        {
+            IEnumerable<Movie> moviesWithoutKeywords = this._unitOfWork.Movies.GetMoviesWithoutKeywords();
+
+            AsyncPolicyWrap policyWrap = GetPolicyWrapFromConfigs(out TimeSpan initialDelay);
+
+            await Task.Delay(initialDelay);
+
+            var errors = new List<string>();
+
+            foreach (Movie movie in moviesWithoutKeywords)
+            {
+                try
+                {
+                    IEnumerable<string> movieKwds = await policyWrap.ExecuteAsync(() => this._movieAPIClient.GetMovieKeywordsAsync(movie.ExternalId));
+                    movie.Keywords = movieKwds.ToList();
+                }
+                catch (RateLimitRejectedException ex)
+                {
+                    errors.Add($"Rate Limit error for {movie.Title} ({movie.ReleaseDate}); Retry after milliseconds: {ex.RetryAfter.TotalMilliseconds}; Message: {ex.Message}");
+                }
+                catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    errors.Add($"Invalid external id for {movie.Title} ({movie.ReleaseDate}): {movie.ExternalId}");
+                }
+                catch (Exception)
+                {
+                    this._unitOfWork.Dispose();
+                    throw;
+                }
+            }
+
+            string dtNow = DateTime.Now.ToString("yyyyMMddHHmmss");
+            PersistErrorInfo($"details_fetcher_errors_KeyWords_{dtNow}.txt", errors);
+            
+            this._unitOfWork.Complete();
+        }
+
+        public async Task PopulateMovieKeywords_OLD()
         {
             IEnumerable<Movie> moviesWithoutKeywords = this._unitOfWork.Movies.GetMoviesWithoutKeywords();
 
@@ -77,6 +124,29 @@ namespace FilmCRUD
             }
 
             this._unitOfWork.Complete();
+        }
+
+        private AsyncPolicyWrap GetPolicyWrapFromConfigs(out TimeSpan initialDelay)
+        {
+            // notice the order of the async policies when calling Policy.WrapAsync:
+            //      outermost (at left) to innermost (at right)
+            IRateLimitPolicyConfig rateLimitConfig = this._appSettingsManager.GetRateLimitPolicyConfig();
+            IRetryPolicyConfig retryConfig = this._appSettingsManager.GetRetryPolicyConfig();
+
+            initialDelay = rateLimitConfig.PerTimeSpan;
+            return Policy.WrapAsync(
+                APIClientPolicyBuilder.GetAsyncRetryPolicy<RateLimitRejectedException>(retryConfig),
+                APIClientPolicyBuilder.GetAsyncRateLimitPolicy(rateLimitConfig)
+            );
+        }
+
+        private void PersistErrorInfo(string filename, IEnumerable<string> errors)
+        {
+            if (!errors.Any()) return;
+
+            string errorsFpath = Path.Combine(this._appSettingsManager.GetWarehouseContentsTextFilesDirectory(), filename);
+            System.Console.WriteLine($"Errors fetching details; see {errorsFpath}");
+            this._fileSystemIOWrapper.WriteAllText(errorsFpath, string.Join("\n\n", errors));
         }
     }
 
