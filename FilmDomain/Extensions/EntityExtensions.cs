78cda0bdcbd412d67753e649e16055013be4861b
mvcb01 <miguelvcbarros01@gmail.com>
2022-09-18 15:47:43 +0100

    changed Movie extension PrettyFormat

--- FilmDomain/Extensions/EntityExtensions.cs
+++ FilmDomain/Extensions/EntityExtensions.cs
@@ -130,7 +130,8 @@ namespace FilmDomain.Extensions
         {
             string _genres = string.Join(" | ", movie.Genres.Select(g => g.Name));
             string _directors = string.Join(", ", movie.Directors.Select(d => d.Name));
-            return string.Join('\n', new string[] {movie.ToString(), _genres, $"Directors: {_directors}", $"IMDB id: {movie.IMDBId}"});
+            string _kwds = movie.Keywords == null ? string.Empty : string.Join(", ", movie.Keywords);
+            return string.Join('\n', new string[] {movie.ToString(), _genres, $"Directors: {_directors}", $"IMDB id: {movie.IMDBId}", $"Keywords: {_kwds}" });
         }
     }
 }
