9fecbe5ec5bc6b6cedaf69b3a24ba94cf2689acb
mvcb01 <miguelvcbarros01@gmail.com>
2022-10-09 13:37:51 +0100

    formatting

--- FilmCRUD/Program.cs
+++ FilmCRUD/Program.cs
@@ -234,6 +234,7 @@ namespace FilmCRUD
             {
                 Console.WriteLine("No action requested...");
             }
+            Console.WriteLine("------------");
         }
 
         private static void HandleScanMoviesOptions(ScanMoviesOptions opts, ServiceProvider serviceProvider)
