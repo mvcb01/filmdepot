5b10783b029e556c95b8c73a9b0703fd5548ce9e
mvcb01 <miguelvcbarros01@gmail.com>
2022-10-17 20:16:30 +0100

    whitespace

--- FilmCRUD/Program.cs
+++ FilmCRUD/Program.cs
@@ -99,7 +99,7 @@ namespace FilmCRUD
                     Log.Error("Should be a date with format yyyyMMdd: {VisitDateString}", visitDateString);
                     throw new FormatException(visitDateString);
                 }
-                
+
                 // default MinimumLevel is Information
                 return new LoggerConfiguration()
                     .WriteTo.File(
@@ -552,4 +552,5 @@ namespace FilmCRUD
         }
 
     }
+
 }
