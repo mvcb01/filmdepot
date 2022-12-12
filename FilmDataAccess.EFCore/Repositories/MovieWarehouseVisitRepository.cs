966f011cee2c555c1ee57d1e6a14f984678d76e8
mvcb01 <miguelvcbarros01@gmail.com>
2022-12-12 19:55:38 +0000

    refactoring IMovieWarehouseVisitRepository implementation

--- FilmDataAccess.EFCore/Repositories/MovieWarehouseVisitRepository.cs
+++ FilmDataAccess.EFCore/Repositories/MovieWarehouseVisitRepository.cs
@@ -8,25 +8,21 @@ namespace FilmDataAccess.EFCore.Repositories
 {
     public class MovieWarehouseVisitRepository : GenericRepository<MovieWarehouseVisit>, IMovieWarehouseVisitRepository
     {
+        public MovieWarehouseVisitRepository(SQLiteAppContext context) : base(context) { }
 
-        public MovieWarehouseVisitRepository(SQLiteAppContext context) : base(context)
-        {
-        }
+        public MovieWarehouseVisit GetClosestMovieWarehouseVisit() => GetClosestMovieWarehouseVisit(DateTime.UtcNow);
 
-        public MovieWarehouseVisit GetClosestMovieWarehouseVisit()
-        {
-            return GetClosestMovieWarehouseVisit(DateTime.UtcNow);
-        }
+        public IEnumerable<DateTime> GetVisitDates() => this._context.MovieWarehouseVisits.Select(v => v.VisitDateTime);
 
         public MovieWarehouseVisit GetClosestMovieWarehouseVisit(DateTime dt)
         {
-            IEnumerable<DateTime> allDatetimes = _context.MovieWarehouseVisits.Select(visit => visit.VisitDateTime).Distinct();
+            IEnumerable<DateTime> allDatetimes = this._context.MovieWarehouseVisits.Select(visit => visit.VisitDateTime).Distinct();
             return Find(visit => visit.VisitDateTime == GetClosestDatetime(allDatetimes, dt)).First();
         }
 
         public MovieWarehouseVisit GetPreviousMovieWarehouseVisit(MovieWarehouseVisit visit)
         {
-            return _context.MovieWarehouseVisits
+            return this._context.MovieWarehouseVisits
                 .Where(v => v.VisitDateTime < visit.VisitDateTime)
                 .OrderByDescending(v => v.VisitDateTime)
                 .FirstOrDefault();
@@ -40,11 +36,6 @@ namespace FilmDataAccess.EFCore.Repositories
             }
 
             return allDateTimes.OrderBy(_dt => Math.Abs((_dt - dt).Ticks)).First();
-        }
-
-        public IEnumerable<DateTime> GetVisitDates()
-        {
-            return _context.MovieWarehouseVisits.Select(v => v.VisitDateTime);
         }
     }
 }
