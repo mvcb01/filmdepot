748c150ffd1a6061146e4169a688cb7deb7fa1c4
mvcb01 <miguelvcbarros01@gmail.com>
2022-12-14 18:55:23 +0000

    docs for GeneralScanManager

--- FilmCRUD/GeneralScanManager.cs
+++ FilmCRUD/GeneralScanManager.cs
@@ -1,22 +1,34 @@
 using System;
-using System.Collections.Generic;
 using FilmDomain.Entities;
-using FilmDomain.Extensions;
 using FilmDomain.Interfaces;
 
 namespace FilmCRUD
 {
-    // to provide some useful methods to other Scan classes
+    /// <summary>
+    /// Provides useful methods to derived scan classes.
+    /// </summary>
     public class GeneralScanManager
     {
         protected readonly IUnitOfWork UnitOfWork;
 
         public GeneralScanManager(IUnitOfWork unitOfWork) => this.UnitOfWork = unitOfWork;
 
+        /// <summary>
+        /// Finds and returns the <see cref="MovieWarehouseVisit"/> in the repository with the closest visit datetime
+        /// to the current datetime.
+        /// </summary>
         public MovieWarehouseVisit GetClosestVisit() => this.UnitOfWork.MovieWarehouseVisits.GetClosestMovieWarehouseVisit();
 
+        /// <summary>
+        /// Finds and returns the <see cref="MovieWarehouseVisit"/> in the repository with the closest visit datetime to the provided <paramref name="dt"/>.
+        /// </summary>
         public MovieWarehouseVisit GetClosestVisit(DateTime dt) => this.UnitOfWork.MovieWarehouseVisits.GetClosestMovieWarehouseVisit(dt);
 
+        /// <summary>
+        /// Finds and returns the most recent <see cref = "MovieWarehouseVisit"/> in the repository among those
+        /// with an older visit datetime than the provided <paramref name="visit"/>. Returns <see langword="null"/> if no such
+        /// visit is found.
+        /// </summary>
         public MovieWarehouseVisit GetPreviousVisit(MovieWarehouseVisit visit) => this.UnitOfWork.MovieWarehouseVisits.GetPreviousMovieWarehouseVisit(visit);
     }
 }
